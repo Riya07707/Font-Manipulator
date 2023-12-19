7f6cf28646ecfee5fb39f0dbc48c1b0a3484ff6a
Riya07707 <113397550+Riya07707@users.noreply.github.com>
2023-12-18 19:39:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,123);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+}
+
+function draw() {
+    background('#E6E6FA');
+ }
+
+ function modelLoaded() {
+    console.log('POse net is initialized');
+ }
+
+ function gotPoses(results) {
+    if(results.length>0) {
+        console.log(results);
+
+    }
+ }
